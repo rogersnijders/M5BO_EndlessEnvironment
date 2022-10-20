fadd024653c770a03e24709ac2d1483c64f1f9f8
Roger Snijders <r.snijders@ma-web.nl>
2022-10-20 18:04:15 +0200

    MoveScript
    MoveScript toegepast op Template en eigen scene

--- Assets/Scripts/PlatformerPlayerMovement.cs
+++ Assets/Scripts/PlatformerPlayerMovement.cs
@@ -5,91 +5,84 @@ using UnityEngine;
 public class PlatformerPlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private CapsuleCollider2D cc;
     private bool Grounded;
     private bool isJumping;
-    
-
-    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-
+    private bool m_FacingRight = true; // For determining which way the player is currently facing.
     private Animator anim;
 
     [SerializeField] private float MovementSpeed;
     [SerializeField] private float JumpSpeed;
-
     [SerializeField] private float RayLength;
     [SerializeField] private float RayPositionOffset;
 
-    Vector3 RayPositionCenter;
-    Vector3 RayPositionLeft;
-    Vector3 RayPositionRight;
-
+    Vector2 RayPositionCenter;
+    Vector2 RayPositionLeft;
+    Vector2 RayPositionRight;
 
     RaycastHit2D[] GroundHitsCenter;
     RaycastHit2D[] GroundHitsLeft;
     RaycastHit2D[] GroundHitsRight;
-
     RaycastHit2D[][] AllRaycastHits = new RaycastHit2D[3][];
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        cc = GetComponent<CapsuleCollider2D>();
-
     }
 
     private void Update()
     {
-      Movement();
-      Jump();
+        Movement();
+        Jump();
     }
 
     private void Movement()
     {
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            rb.velocity = new Vector2(MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
+            rb.velocity =
+                new Vector2(MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
             anim.SetBool("isWalking", true);
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            rb.velocity = new Vector2(-MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
+            rb.velocity =
+                new Vector2(-MovementSpeed * Time.fixedDeltaTime,
+                    rb.velocity.y);
             anim.SetBool("isWalking", true);
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
             anim.SetBool("isWalking", false);
-
         }
 
         if (Input.GetKey(KeyCode.Space) && Grounded)
         {
-          //anim.SetBool("isJumping", false);
-          rb.velocity = new Vector2(rb.velocity.x, 0);
-          rb.velocity = new Vector2(rb.velocity.x, JumpSpeed);
-
+            //anim.SetBool("isJumping", false);
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.velocity = new Vector2(rb.velocity.x, JumpSpeed);
         }
-        if (Grounded) {
-          anim.SetBool ("isJumping", false);
 
-          }
-          else {
-            anim.SetBool ("isJumping", true);
-          }
+        if (Grounded)
+        {
+            anim.SetBool("isJumping", false);
+        }
+        else
+        {
+            anim.SetBool("isJumping", true);
+        }
 
         // If the input is moving the player right and the player is facing left...
         if (rb.velocity.x > 0 && !m_FacingRight)
         {
-          // ... flip the player.
-          Flip();
-        }
-          // Otherwise if the input is moving the player left and the player is facing right...
+            // ... flip the player.
+            Flip();
+        } // Otherwise if the input is moving the player left and the player is facing right...
         else if (rb.velocity.x < 0 && m_FacingRight)
         {
-          // ... flip the player.
-          Flip();
+            // ... flip the player.
+            Flip();
         }
     }
 
@@ -103,11 +96,15 @@ public class PlatformerPlayerMovement : MonoBehaviour
 
     private void Jump()
     {
-        RayPositionCenter = transform.position + new Vector3(0, RayLength * .5f, 0);
-        RayPositionLeft = transform.position + new Vector3(-RayPositionOffset, RayLength * .5f, 0);
-        RayPositionRight = transform.position + new Vector3(RayPositionOffset, RayLength * .5f, 0);
+        var position = transform.position;
 
-        GroundHitsCenter = Physics2D.RaycastAll(RayPositionCenter, -Vector2.up, RayLength);
+        // RayPositionCenter = transform.position + new Vector3(0, RayLength * .5f, 0);
+        // RayPositionLeft = transform.position + new Vector3(-RayPositionOffset, RayLength * .5f, 0);
+        // RayPositionRight = transform.position + new Vector3(RayPositionOffset, RayLength * .5f, 0);
+        RayPositionLeft = new Vector2(position.x - RayPositionOffset, position.y);
+        RayPositionRight = new Vector2(position.x + RayPositionOffset, position.y);
+
+        GroundHitsCenter = Physics2D.RaycastAll(position, -Vector2.up, RayLength);
         GroundHitsLeft = Physics2D.RaycastAll(RayPositionLeft, -Vector2.up, RayLength);
         GroundHitsRight = Physics2D.RaycastAll(RayPositionRight, -Vector2.up, RayLength);
 
@@ -123,21 +120,21 @@ public class PlatformerPlayerMovement : MonoBehaviour
     }
 
     private bool GroundCheck(RaycastHit2D[][] GroundHits)
-      {
+    {
         foreach (RaycastHit2D[] HitList in GroundHits)
         {
-          foreach (RaycastHit2D hit in HitList)
-          {
-            if (hit.collider != null)
+            foreach (RaycastHit2D hit in HitList)
             {
-              if (hit.collider.tag != "PlayerCollider")
-              {
-                return true;
-              }
+                if (hit.collider != null)
+                {
+                    if (hit.transform != transform)
+                    {
+                        return true;
+                    }
+                }
             }
-          }
         }
-        return false;
-      }
 
+        return false;
     }
+}
